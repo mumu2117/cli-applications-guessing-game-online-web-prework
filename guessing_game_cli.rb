a1699981aad9f1756dafb7daaa4b6b80acebd7fd
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 22:59:27 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,14 @@
-# Code your solution here!
+ddef run_guessing_game
+  num_guess = ""
+  until num_guess == "exit"
+    puts "Guess a number between 1 and 6."
+    num_guess = gets.chomp
+    random_num = rand(1..6).to_s
+    if num_guess == random_num
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{random_num}."
+    end
+  end
+  puts "Goodbye!"
+end
